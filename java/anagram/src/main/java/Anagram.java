7917b292d428714ad216247b2e3fdd71eebf76d3
Julia Wilson <julia.wilson@bbc.co.uk>
2016-07-02 19:22:34 +0100

    Anagram complete

---
+++ java/anagram/src/main/java/Anagram.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Anagram {
+  private final String word;
+  private final char[] canonical;
+
+  public Anagram(String word) {
+    this.word = word;
+    this.canonical = this.canonicalForm(word);
+  }
+
+  public List<String> match(List<String> possibleAnagrams) {
+    return possibleAnagrams
+            .stream()
+            .filter( p -> this.isAnagramOf(p) )
+            .collect(Collectors.toList());
+  }
+
+  private boolean isAnagramOf(String another) {
+    return !this.word.equalsIgnoreCase(another) && Arrays.equals(this.canonical, canonicalForm(another));
+  }
+
+  private char[] canonicalForm(String word) {
+    char[] alphabetised = word.toLowerCase().toCharArray();
+    Arrays.sort(alphabetised);
+    return alphabetised;
+  }
+}
