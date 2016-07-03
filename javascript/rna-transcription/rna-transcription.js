1c8da3ef70a12443884d038d204c2ddc124be1e1
Julia Wilson <julia.wilson@bbc.co.uk>
2016-07-03 12:31:50 +0100

    Use const in place of var.

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,18 +1,18 @@
 module.exports = function() {
-  var DNA_TO_RNA_MAP = {
+  const DNA_TO_RNA_MAP = {
     'A': 'U',
     'G': 'C',
     'C': 'G',
     'T': 'A'
-  };
+  }
 
   function toRna(dna) {
     return dna.split('')
       .map(nucleotide => DNA_TO_RNA_MAP[nucleotide])
-      .join('');
+      .join('')
   }
 
   return {
     toRna: toRna
   }
-};
+}
