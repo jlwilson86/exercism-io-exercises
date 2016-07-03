518753714196ded31ffe0e4e99d7203e70a5594d
Julia Wilson <julia.wilson@bbc.co.uk>
2016-07-03 12:31:23 +0100

    Use const in place of var.

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,5 +1,5 @@
 module.exports = function(startDate) {
-  var ONE_BILLION_SECONDS_IN_MILLIS = Math.pow(10, 9) * 1000;
+  const ONE_BILLION_SECONDS_IN_MILLIS = Math.pow(10, 9) * 1000;
 
   function date() {
     return new Date(startDate.getTime() + ONE_BILLION_SECONDS_IN_MILLIS);
